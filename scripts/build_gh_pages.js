a2985d7b4cb49d6214381e8cd22e0f9e93ef2ff3
Brandon K. Waltz <bwaltz6@gmail.com>
2022-02-22 12:53:04 -0500

    actions

---
+++ scripts/build_gh_pages.js
@@ -0,0 +1,38 @@
+const { exec } = require('child_process');
+
+function getArgs() {
+    const args = {};
+    process.argv.slice(2, process.argv.length).forEach((arg) => {
+        // long arg
+        if (arg.slice(0, 2) === '--') {
+            const longArg = arg.split('=');
+            const longArgFlag = longArg[0].slice(2, longArg[0].length);
+            const longArgValue = longArg.length > 1 ? longArg[1] : true;
+            args[longArgFlag] = longArgValue;
+        }
+        // flags
+        else if (arg[0] === '-') {
+            const flags = arg.slice(1, arg.length).split('');
+            flags.forEach((flag) => {
+                args[flag] = true;
+            });
+        }
+    });
+    return args;
+}
+
+const args = getArgs();
+
+console.log(args);
+
+exec('pwd', (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
