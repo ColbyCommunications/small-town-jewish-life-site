afad1d5b6ae4db601d0697ddd4ff6d69e865c5db
Brandon K. Waltz <bwaltz6@gmail.com>
2022-02-25 14:56:02 -0500

    actions

--- scripts/sync_platform_db.sh
+++ scripts/sync_platform_db.sh
@@ -3,15 +3,15 @@
 BRANCH=$1
 ID=$2
 
-CHILDSIZE=$(~/.platformsh/bin/platform db:size --environment=$BRANCH --format=plain --columns=used --no-header | sed 's/[^0-9]*//g')
-PARENT=$(~/.platformsh/bin/platform environment:info --format=plain parent)
-PARENTSIZE=$(~/.platformsh/bin/platform db:size --environment=$PARENT --format=plain --columns=used --no-header | sed 's/[^0-9]*//g')
+CHILDSIZE=$(platform db:size --environment=$BRANCH --format=plain --columns=used --no-header | sed 's/[^0-9]*//g')
+PARENT=$(platform environment:info --format=plain parent)
+PARENTSIZE=$(platform db:size --environment=$PARENT --format=plain --columns=used --no-header | sed 's/[^0-9]*//g')
 
 echo $PARENTSIZE
 echo $CHILDSIZE
 
 if [[ $PARENTSIZE == $CHILDSIZE ]] ; then
-	~/.platformsh/bin/platform sync data --project=$ID --environment=$BRANCH --yes
+	platform sync data --project=$ID --environment=$BRANCH --yes
     echo "Synching Databases!"
 else
 	echo "Databases different! Not syncing"
