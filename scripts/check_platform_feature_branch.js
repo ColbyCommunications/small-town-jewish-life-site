a4ee974aa72c2ce82383c8055bf7caa2ab06a08d
Brandon K. Waltz <bwaltz6@gmail.com>
2022-02-28 10:16:36 -0500

    actions

--- scripts/check_platform_feature_branch.js
+++ scripts/check_platform_feature_branch.js
@@ -1,5 +1,29 @@
 const { exec } = require('child_process');
 
+function getArgs() {
+    const args = {};
+    process.argv.slice(2, process.argv.length).forEach((arg) => {
+        // long arg
+        if (arg.slice(0, 2) === '--') {
+            const longArg = arg.split('=');
+            const longArgFlag = longArg[0].slice(2, longArg[0].length);
+            const longArgValue = longArg.length > 1 ? longArg[1] : true;
+            args[longArgFlag] = longArgValue;
+        }
+        // flags
+        else if (arg[0] === '-') {
+            const flags = arg.slice(1, arg.length).split('');
+            flags.forEach((flag) => {
+                args[flag] = true;
+            });
+        }
+    });
+    return args;
+}
+
+// get args
+const args = getArgs();
+
 exec(
     '~/.platformsh/bin/platform environment:list --columns=ID --format=plain --no-header',
     (error, stdout, stderr) => {
@@ -11,6 +35,26 @@ exec(
             console.log(`stderr: ${stderr}`);
             return;
         }
-        console.log(`stdout: ${stdout}`);
+
+        if (stdout.includes(args.branch)) {
+            
+            exec(
+                `~/.platformsh/bin/platform environment:activate ${args.branch} --project=${args.id}`,
+                (error, stdout, stderr) => {
+                    if (error) {
+                        console.log(`error: ${error.message}`);
+                        return;
+                    }
+                    if (stderr) {
+                        console.log(`stderr: ${stderr}`);
+                        return;
+                    }
+            
+                    if (stdout.includes(args.branch)) {
+                        ~/.platformsh/bin/platform environment:activate ${GITHUB_REF#refs/heads/} --project=${{ secrets.PLATFORM_PROJECT_ID }}
+                    }
+                }
+            );
+        }
     }
 );
