46e10811e6a1d5fcd4612c9605b7ec69ee05b16c
Brandon K. Waltz <bwaltz6@gmail.com>
2022-03-04 14:46:20 -0500

    actions

--- scripts/sitemap.sh
+++ scripts/sitemap.sh
@@ -1,4 +1,6 @@
 EDGE_HOSTNAME=$(~/.platformsh/bin/platform environment:info edge_hostname)
 JQ="[https://${EDGE_HOSTNAME}]"
+echo $JQ
 echo $(cat .github/sitemap.json | jq --arg JQ '.urls |= $JQ') > .github/sitemap.json
+cat .github/sitemap.json
 flatten_sitemap --sitemap https://$EDGE_HOSTNAME/sitemap_index.xml --config .github/sitemap.json --limit 20 --randomize
