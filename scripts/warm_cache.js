ad3b9133a48e7201c8dfcb73d7542bdbf3638f43
Brandon K. Waltz <bwaltz6@gmail.com>
2022-03-07 14:41:01 -0500

    actions

--- scripts/warm_cache.js
+++ scripts/warm_cache.js
@@ -6,8 +6,10 @@ fs.readFile('.github/sitemap.json', (err, data) => {
     let sitemap = JSON.parse(data);
 
     sitemap.urls.forEach((url) => {
+        let options = { url, agent: 'node-fetch/1.0 (+https://github.com/bitinn/node-fetch)' };
         https
-            .get(url, (res) => {
+            .get(options, (res) => {
+                console.log('headers:', res.headers);
                 console.log(res.headers['cf-cache-status']);
             })
             .on('error', (err) => {
