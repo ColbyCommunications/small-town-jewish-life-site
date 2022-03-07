2c6a59deebd9f36bd7eb6aa049b1260027b05c6e
Brandon K. Waltz <bwaltz6@gmail.com>
2022-03-07 16:36:11 -0500

    actions

--- scripts/warm_cache.js
+++ scripts/warm_cache.js
@@ -1,20 +1,25 @@
 const https = require('https');
 const fs = require('fs');
+const superagent = require('superagent');
 
 fs.readFile('.github/sitemap.json', (err, data) => {
     if (err) throw err;
     let sitemap = JSON.parse(data);
 
     sitemap.urls.forEach((url) => {
-        // run twice for good measure
-        for (let i = 1; i <= 2; i++) {
-            https
-                .get(url, (res) => {
-                    console.log(res.headers['cf-cache-status'] + `: ${url}`);
-                })
-                .on('error', (err) => {
-                    console.log(err.message);
-                });
-        }
+        // https
+        //     .get(url, (res) => {
+        //         console.log(res.headers['cf-cache-status'] + `: ${url}`);
+        //     })
+        //     .on('error', (err) => {
+        //         console.log(err.message);
+        //     });
+
+        superagent
+            .get(url)
+            .set('user-agent', 'node-fetch/1.0 (+https://github.com/bitinn/node-fetch)')
+            .end((err, res) => {
+                console.log(res.headers['cf-cache-status'] + `: ${url}`);
+            });
     });
 });
