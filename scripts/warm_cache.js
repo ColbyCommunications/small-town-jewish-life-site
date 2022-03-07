dfc4fe9164f21bda3e691b017d4bc174b24dfb22
Brandon K. Waltz <bwaltz6@gmail.com>
2022-03-07 14:05:15 -0500

    actions

--- scripts/warm_cache.js
+++ scripts/warm_cache.js
@@ -1,11 +1,11 @@
 const { fetch } = require('node-fetch');
 const fs = require('fs');
 
-fs.readFile('.github/sitemap.json', (err, data) => {
+fs.readFile('.github/sitemap.json', async (err, data) => {
     if (err) throw err;
     let sitemap = JSON.parse(data);
 
-    sitemap.urls.forEach((url) => {
+    sitemap.urls.forEach(async (url) => {
         const response = await fetch(url);
         const data = await response.json();
     });
