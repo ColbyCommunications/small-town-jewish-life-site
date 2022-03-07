68c9f125d14f341286f0fad49e0beae9f78f3fed
Brandon K. Waltz <bwaltz6@gmail.com>
2022-03-07 15:03:50 -0500

    actions

--- scripts/warm_cache.js
+++ scripts/warm_cache.js
@@ -1,24 +1,18 @@
 const https = require('https');
 const fs = require('fs');
-const axios = require('axios');
 
 fs.readFile('.github/sitemap.json', (err, data) => {
     if (err) throw err;
     let sitemap = JSON.parse(data);
 
     sitemap.urls.forEach((url) => {
-        axios
-            .get(url, { headers: { 'User-Agent': 'colby-gh' } })
-            .then(function (response) {
-                // handle success
-                console.log(response);
+        https
+            .get(url, (res) => {
+                console.log('headers:', res.headers);
+                console.log(res.headers['cf-cache-status']);
             })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .then(function () {
-                // always executed
+            .on('error', (err) => {
+                console.log(err.message);
             });
     });
 });
