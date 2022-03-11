079b989d408258d86b5caf7295fc61484283db43
Brandon K. Waltz <bwaltz6@gmail.com>
2022-03-11 16:12:28 -0500

    actions

--- scripts/warm_cache.js
+++ scripts/warm_cache.js
@@ -1,4 +1,3 @@
-const https = require('https');
 const fs = require('fs');
 const superagent = require('superagent');
 
@@ -7,14 +6,6 @@ fs.readFile('.github/sitemap.json', (err, data) => {
     let sitemap = JSON.parse(data);
 
     sitemap.urls.forEach((url) => {
-        // https
-        //     .get(url, (res) => {
-        //         console.log(res.headers['cf-cache-status'] + `: ${url}`);
-        //     })
-        //     .on('error', (err) => {
-        //         console.log(err.message);
-        //     });
-
         superagent
             .get(url)
             .set('user-agent', 'colby-github')
