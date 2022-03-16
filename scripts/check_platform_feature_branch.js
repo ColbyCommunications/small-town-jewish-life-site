097283c6208858f2edc5bde81404668e4e11286a
Brandon K. Waltz <bwaltz6@gmail.com>
2022-03-16 10:39:51 -0400

    actions

--- scripts/check_platform_feature_branch.js
+++ scripts/check_platform_feature_branch.js
@@ -28,33 +28,24 @@ exec(
     '~/.platformsh/bin/platform environment:list --columns=ID --format=plain --no-header',
     (error, stdout, stderr) => {
         if (error) {
-            console.log(`error: ${error.message}`);
             return;
         }
         if (stderr) {
-            console.log(`stderr: ${stderr}`);
             return;
         }
 
-        console.log(stdout.includes(args.branch));
         if (!stdout.includes(args.branch)) {
             execSync(
                 ` ~/.platformsh/bin/platform environment:branch ${args.branch} ${args.branchFrom}`,
                 (error, stdout, stderr) => {
                     if (error) {
-                        console.log(`error: ${error.message}`);
                         return;
                     }
                     if (stderr) {
-                        console.log(`stderr: ${stderr}`);
                         return;
                     }
-
-                    console.log(stdout);
                 }
             );
         }
-
-        return true;
     }
 );
