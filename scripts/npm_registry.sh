14533aced3840c345720348474edcf1b04f48255
Brandon K. Waltz <bwaltz6@gmail.com>
2022-04-13 12:15:16 -0400

    test

--- scripts/npm_registry.sh
+++ scripts/npm_registry.sh
@@ -3,5 +3,5 @@
 mv /app/.npmrc .npmrc-gh
 cat > /app/.npmrc <<EOL
 //npm.pkg.github.com/:_authToken=${WEBMASTER_GITHUB_ACTIONS_TOKEN}
-registry=https://npm.pkg.github.com/ColbyCommunications
+@ColbyCommunications:registry=https://npm.pkg.github.com
 EOL
