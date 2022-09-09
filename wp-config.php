cb85e0833603f68c680e745329b442af59c3e727
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2022-09-09 15:22:04 -0400

    add support for elementor custom icons

--- wp-config.php
+++ wp-config.php
@@ -199,6 +199,9 @@ if (!defined('ABSPATH') ) {
     define('ABSPATH', dirname(__FILE__) . '/');
 }
 
+// Elementor icon support
+define('FS_METHOD', 'direct');
+
 /**
  * Sets up WordPress vars and included files.
  * Moved to ./web/wp-config.php
